162bb50b1362eb592ae05f79f963ffc9e1759c1d
moti9 <motiky.iiitag@gmail.com>
2023-05-22 21:06:36 +0530

    First commit

--- EmailCampaign/urls.py
+++ EmailCampaign/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("subscribe/", views.SubscribeView.as_view(), name="subscribe"),
     path("unsubscribe/", views.UnsubscribeView.as_view(), name="unsubscribe"),
     path("mail/", views.send_campaigns, name="send_compaigns"),
-    path("", views.home, name="home"),
 ]
